45f12713a59600ae72747dcb79658e067b180b8d
lisssb <liss.sb@gmail.com>
2018-11-10 22:55:12 +0100

    add jsp template

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -2,18 +2,26 @@ package com.example.demo;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+	Logger log = LoggerFactory.getLogger(MainController.class);
 	
 	@RequestMapping("/hi/{name}")
-	public  String hiThere( Map model, @PathVariable String name) {
-//		public @ResponseBody String hiThere( Map model, @PathVariable String name) {
-		model.put("name", name);
+	public  String hiThere( @PathVariable String name, ModelMap model) {
+
+		//		public @ResponseBody String hiThere( Map model, @PathVariable String name) {
+//		model.put("name", name);
+		model.addAttribute("name", name);
+		log.info("uuuu {}", name);
 		return "hello";
 	}
 }
